deaf0e0bd67bc5fea2699613b71f7547c9194a39
Vincent Dankbaar <38166547+vdankbaar@users.noreply.github.com>
2021-09-10 17:17:19 +0200

    Help things aren't working

--- WarmupProject/SimonScript/SimonScript.ino
+++ WarmupProject/SimonScript/SimonScript.ino
@@ -25,15 +25,41 @@ const char* ssid = "NDL_24G"; // name of local WiFi network in the NDL
 const char* password = "RT-AC66U"; // the password of the WiFi network
 MDNSResponder mdns;
 ESP8266WebServer server(LISTEN_PORT);
-String startPage = "<html><head><title>Simon commands</title></head><body><h3>Simon is impatiënt and has given up on asking politely!</h3><p>He now only commands; Do what he says and be rewarded!</p><a href=\"begin\"><button style=\"background-color:blue;color:white;\">Begin</button></a></body></html>";
+String startPage = "<html><head><title>Simon commands</title></head><body><h3>Simon is impatient and has given up on asking politely!</h3><p>He now only commands; Do what he says and be rewarded!</p><a href=\"begin\"><button style=\"background-color:blue;color:white;\">Begin</button></a></body></html>";
 String readyPage = "<html><head><title>Simon commands</title></head><body><a href=\"check\"><button style=\"background-color:blue;color:white;\">Check your sequence</button></a></body></html>";
+String allInstructions[] = {"Simon says move to the right",
+                              "Simon says move to the left",
+                              "Simon says move up",
+                              "Simon says move down",
+                              "Simon says move forward",
+                              "Simon says move backward",
+                              "Simon says wave"
+                              /*
+                              ,
+                              "Simon says move clockwise",
+                              "Simon says move counterclockwise",
+      
+                              //
+                              "Move to the right",
+                              "Move to the left",
+                              "Move clockwise",
+                              "Move counterclockwise",
+                              "Move up",
+                              "Move down",
+                              "Move forward",
+                              "Move backward",
+                              "Wave*/
+                              };
+
 String screenTitle = "";
 bool simonWatching = false;
 
-int maxInstructions = 7;
+const int maxInstructions = 4;
+const int totalInstructions = 7;
 int randomisedInts[maxInstructions];
 int performedInstructions[maxInstructions];
 int performedInstructionCount = 0;
+int amountOfInstructions;
 
 void writeToScreen(String text, int fontsize) {
   display.clearDisplay();
@@ -49,8 +75,8 @@ void writeToScreen(String text, int fontsize) {
 }
 
 void setup() {
-  Serial.begin(115200); // the serial speed
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C address
+  Serial.begin(115200); // the serial speed
   writeToScreen("Booting", 2);
   pinMode(LED_BUILTIN, OUTPUT); // the LED
   digitalWrite(LED_BUILTIN, HIGH); // the actual status is inverted
@@ -77,15 +103,17 @@ void setup() {
   });
 
   server.on("/ready" , [ ](){
+    simonWatching = true;
     server.send(200, "text/html" , readyPage);
     digitalWrite(LED_BUILTIN, LOW);
   });
 
   server.on("/check" , [ ](){
+    performedInstructionCount = 0;
+    simonWatching = false;
     server.send(200, "text/html" , generateCheckPage());
     digitalWrite(LED_BUILTIN, LOW);
   });
-
   uint8_t error = paj7620Init( ); // initialize Paj7620 registers
   if (error) {
     Serial.print("INIT ERROR,CODE: ");
@@ -99,7 +127,8 @@ void setup() {
 }
 void loop() {
   server.handleClient();
-  handleGestures()
+  if (simonWatching && performedInstructionCount < maxInstructions)
+    handleGestures();
 }
 
 void handleGestures()
@@ -110,34 +139,52 @@ void handleGestures()
     switch (data) {
       case GES_RIGHT_FLAG:
         Serial.println("Right");
+        performedInstructions[performedInstructionCount] = 0;
+        performedInstructionCount++;
         break;
       case GES_LEFT_FLAG:
         Serial.println("Left");
+        performedInstructions[performedInstructionCount] = 1;
+        performedInstructionCount++;
         break;
       case GES_UP_FLAG:
         Serial.println("Up");
+        performedInstructions[performedInstructionCount] = 2;
+        performedInstructionCount++;
         break;
       case GES_DOWN_FLAG:
         Serial.println("Down");
+        performedInstructions[performedInstructionCount] = 3;
+        performedInstructionCount++;
         break;
       case GES_FORWARD_FLAG:
         Serial.println("Forward");
+        performedInstructions[performedInstructionCount] = 4;
+        performedInstructionCount++;
         delay(GES_QUIT_TIME);
         break;
       case GES_BACKWARD_FLAG:
         Serial.println("Backward");
+        performedInstructions[performedInstructionCount] = 5;
+        performedInstructionCount++;
         delay(GES_QUIT_TIME);
         break;
       case GES_CLOCKWISE_FLAG:
         Serial.println("Clockwise");
+        performedInstructions[performedInstructionCount] = 7;
+        performedInstructionCount++;
         break;
       case GES_COUNT_CLOCKWISE_FLAG:
-        Serial.println("anti-clockwise");
+        Serial.println("Counter Clockwise");
+        performedInstructions[performedInstructionCount] = 8;
+        performedInstructionCount++;
         break;
       default:
         paj7620ReadReg(I2C_ADDRESS2, 1, &data);
         if (data == GES_WAVE_FLAG) {
-          Serial.println("wave");
+          Serial.println("Wave");
+          performedInstructions[performedInstructionCount] = 6;
+          performedInstructionCount++;
         } else {
           Serial.print(".");
         }
@@ -148,9 +195,22 @@ void handleGestures()
 }
 
 String generateCheckPage() {
-  String message;
-  if 
-  return "<html><head><title>Simon commands</title></head><body><h1>"+message+"</h1></body></html>"
+  String performed = "";
+  String expected = "";
+  for(int i=0; i<amountOfInstructions; i++){
+    //expected += allInstructions[randomisedInts[i]];
+    expected += (char) (randomisedInts[i]+48);
+    if(i != amountOfInstructions)
+      expected += "-";
+  }
+
+  for(int i=0; i<performedInstructionCount; i++){
+    //performed += allInstructions[performedInstructions[i]];
+    performed += (char) (performedInstructions[i]+48);
+    if(i != performedInstructionCount)
+      performed += "-";
+  }
+  return "<html><head><title>Simon commands</title></head><body><h1>"+expected+"</h1><h1>"+performed+"</h1><h1>"+expected.equals(performed)+"</h1></body></html>";
 }
 
 String generateInstructionPage() {
@@ -158,38 +218,10 @@ String generateInstructionPage() {
 }
 
 String generateInstructions() {
-
-  String allInstructions[] = {"Simon says move to the right",
-                              "Simon says move to the left",
-                              "Simon says move up",
-                              "Simon says move down",
-                              "Simon says move forward",
-                              "Simon says move backward",
-                              "Simon says wave"
-                              /*
-                              ,
-                              "Simon says move clockwise",
-                              "Simon says move counterclockwise",
-      
-                              //
-                              "Move to the right",
-                              "Move to the left",
-                              "Move clockwise",
-                              "Move counterclockwise",
-                              "Move up",
-                              "Move down",
-                              "Move forward",
-                              "Move backward",
-                              "Wave*/
-                              };
-  //int instructionCount = sizeof(allInstructions);
   String instructionsString = "";
-
-  randomSeed(analogRead(0));  
-  int amountOfInstructions = random(2, maxInstructions + 1);
-
+  amountOfInstructions = random(2, maxInstructions + 1);
   for(int i=0; i<amountOfInstructions; i++){
-    int randInt = random(maxInstructions);
+    int randInt = random(totalInstructions);
     randomisedInts[i] = randInt;
     instructionsString += allInstructions[randInt];
     if(i != amountOfInstructions)  
